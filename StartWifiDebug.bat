675179255d06522e2a538ab42a75514f6f08e508
Egor <egorshalaev@mail.ru>
2022-08-11 12:14:42 +0300

    start commit

---
+++ StartWifiDebug.bat
@@ -0,0 +1,3 @@
+D:\AllAplications\All_for_Java\All_for_android_app\AndroidSDK\platform-tools\adb.exe tcpip 5555
+D:\AllAplications\All_for_Java\All_for_android_app\AndroidSDK\platform-tools\adb.exe connect 192.168.1.65
+pause
---
+++ app/src/main/java/com/master/esp8266_addressledscontroller/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.master.esp8266_addressledscontroller
+
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.master.esp8266_addressledscontroller.databinding.ActivityMainBinding
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
+/* Для связи с МК по wi-fi надо:
+* 1. Добавить в AndroidManifest разрешение на использование интернета и http.
+* 2. Добавить в build.gradle (Module) библиотеку okhttp3 */
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  request: Request
+    private lateinit var  binding: ActivityMainBinding
+    private lateinit var pref: SharedPreferences
+    private val client = OkHttpClient()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        pref = getSharedPreferences("MyPref", MODE_PRIVATE)
+        onClickSaveIp()
+        getIp()
+        binding.apply {
+            bLed1.setOnClickListener(onClickListener())
+            bLed2.setOnClickListener(onClickListener())
+            bLed3.setOnClickListener(onClickListener())
+        }
+    }
+
+    private fun onClickListener(): View.OnClickListener {
+        return View.OnClickListener {
+            when(it.id) {
+                R.id.bLed1 -> { post("mode1")}
+                R.id.bLed2 -> { post("mode2")}
+                R.id.bLed3 -> { post("mode3")}
+            }
+        }
+    }
+
+    private fun onClickSaveIp() = with(binding) {
+        bSave.setOnClickListener {
+            if(edip.text.isNotEmpty()) saveIp(edip.text.toString())
+        }
+    }
+
+    // Сохранение ip в память
+    private fun saveIp(ip: String) {
+        val editor = pref.edit()
+        editor.putString("ip", ip)
+        editor.apply()
+    }
+
+    // Получение ip из памяти
+    private fun getIp() = with(binding) {
+        val ip = pref.getString("ip", "")
+        if(ip != null) {
+            if(ip.isNotEmpty()) edip.setText(ip)
+        }
+    }
+
+    // Отправка команды на Esp и приём от неё ответа
+    private fun post(post: String) {
+        // Отправляем запрос в отдельном потоке, т.к. это затратная операция
+        Thread {
+            // Формируем запрос вида: http://<destination_ip>/<command>
+            request = Request.Builder().url("http://${binding.edip.text}/effect?ef=$post").build()
+            Log.d("MY_LOGS", "Android: ${request.url()}")
+
+            try{
+                // Отправляем запрос
+                var response = client.newCall(request).execute()
+                if(response.isSuccessful) {
+                    // Получаем ответ
+                    val resultText = response.body()?.string()
+                    Log.d("MY_LOGS", "Esp: $resultText")
+
+                    runOnUiThread{
+                        binding.tvState.text = resultText
+                    }
+                } else {
+                    Log.d("MY_LOGS", "Esp: NO ANSWER!")
+                }
+            } catch(i: IOException){
+                Log.d("MY_LOGS", "EXCEPTION in get wi-fi answer!")
+            }
+
+        }.start()
+    }
+}
