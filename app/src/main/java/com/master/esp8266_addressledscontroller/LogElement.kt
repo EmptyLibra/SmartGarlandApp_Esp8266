ff71dec29013a0a9858d3581b279adf37f7ada18
Egor <egorshalaev@mail.ru>
2022-12-24 19:56:24 +0300

    Добавляем прочие файлы проекта (забыл добавить)

---
+++ app/src/main/java/com/master/esp8266_addressledscontroller/LogElement.kt
@@ -0,0 +1,22 @@
+package com.master.esp8266_addressledscontroller
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
+enum class LogTypes(val text: String){
+    ERROR("Error"), ANDROID_SEND("Android_Send"), ESP_ANSWER("Esp_Answer")
+}
+
+data class LogElement(var Date: LocalTime, var Type: LogTypes, var message: String) {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun toString(): String {
+        val str = "${Date.hour}:${Date.minute}:${Date.second} [${Type.text}]:<br> $message"
+        return when(this.Type){
+            LogTypes.ERROR -> "<font color=\"red\">$str</font><br><br>"
+            LogTypes.ANDROID_SEND -> "<font color=\"blue\">$str</font><br><br>"
+            LogTypes.ESP_ANSWER -> "<font color=\"green\">$str</font><br><br>"
+        }
+    }
+}
